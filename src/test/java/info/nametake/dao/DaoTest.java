a88c4cf953af3965fba837c37b294234b02e5114
nametake <nametake.kyarabuki@gmail.com>
2016-05-19 18:06:51 +0900

    Write Dao test

--- src/test/java/info/nametake/dao/DaoTest.java
+++ src/test/java/info/nametake/dao/DaoTest.java
@@ -1,31 +1,72 @@
 package info.nametake.dao;
 
 import info.nametake.models.User;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
+import org.junit.*;
 
+import java.sql.SQLException;
+
+import static org.hamcrest.CoreMatchers.instanceOf;
+import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
 /**
  * Created by nameki-shogo on 2016/05/19.
  */
-public class DaoTest {
+public class DaoTest extends BaseDBTest {
 
-    @Before
-    public void setUp() throws Exception {
 
+    Dao<User> userDao = null;
+    User user = null;
+
+    @BeforeClass
+    public void createDao() {
+        userDao = DaoManager.createDao(con, User.class);
     }
 
+    @AfterClass
+    public void deleteDao() {
+        userDao = null;
+    }
+
+    /**
+     * ユーザインスタンスの作成
+     */
+    @Before
+    public void createUser() {
+        user = new User();
+        user.setId(10);
+        user.setName("Taro");
+        user.setPassword("pass");
+    }
+
+    /**
+     * ユーザインスタンスを削除
+     */
     @After
-    public void tearDown() throws Exception {
+    public void deleteUser() {
+        user = null;
+    }
+
+    @Test
+    public void testCreateDao() {
+        Dao<User> userDao = DaoManager.createDao(con, User.class);
+        assertThat(userDao, instanceOf(Dao.class));
+    }
+
+    @Test
+    public void testInsert() throws SQLException {
+        fail();
 
     }
 
     @Test
-    public void TestCreateDao() throws Exception {
+    public void testDelete() throws SQLException {
         fail();
+
     }
 
-
+    @Test
+    public void testSelect() throws SQLException {
+        fail();
+    }
 }
