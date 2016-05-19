014e06f2167f078b63f985bdc6d4dc4bf1db710b
nametake <nametake.kyarabuki@gmail.com>
2016-05-20 01:21:04 +0900

    Add constructor to StmtExecutorImpl

--- src/main/java/info/nametake/stmt/StatementExecutorImpl.java
+++ src/main/java/info/nametake/stmt/StatementExecutorImpl.java
@@ -2,8 +2,17 @@ package info.nametake.stmt;
 
 import info.nametake.stmt.StatementExecutor;
 
+import java.sql.Connection;
+
 /**
  * Created by shogo on 2016/05/20.
  */
 public class StatementExecutorImpl<T> implements StatementExecutor<T>{
+    protected Connection connection;
+    protected Class<T> clazz;
+
+    public StatementExecutorImpl(Connection connection, Class<T> clazz) {
+        this.connection = connection;
+        this.clazz = clazz;
+    }
 }
