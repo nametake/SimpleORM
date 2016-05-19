1a789cbd29b26824e692164ac1358f2b07e04bc0
nametake <nametake.kyarabuki@gmail.com>
2016-05-19 11:45:16 +0900

    Add SLQBuilder interface

--- src/main/java/info/nametake/dao/Dao.java
+++ src/main/java/info/nametake/dao/Dao.java
@@ -1,8 +1,15 @@
 package info.nametake.dao;
 
+import java.util.List;
+
 /**
  * Created by shogo on 2016/05/19.
  */
 public interface Dao<T> {
-
+    public List<T> select(T data);
+    public T selectById(int id);
+    public List<T> selectAll();
+    public int update(T data);
+    public int insert(T data);
+    public int delete(T data);
 }
--- src/main/java/info/nametake/stmt/StatementExecutor.java
+++ src/main/java/info/nametake/stmt/StatementExecutor.java
@@ -4,4 +4,5 @@ package info.nametake.stmt;
  * Created by nameki-shogo on 2016/05/19.
  */
 public interface StatementExecutor {
+
 }
