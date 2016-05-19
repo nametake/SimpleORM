31e7e090e59c594e60caa6f4bd8c57793687ec4e
nametake <nametake.kyarabuki@gmail.com>
2016-05-19 11:02:51 +0900

    Define annotation

--- src/main/java/info/nametake/db/DatabaseField.java
+++ src/main/java/info/nametake/db/DatabaseField.java
@@ -1,7 +1,16 @@
 package info.nametake.db;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.TYPE;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 /**
  * Created by nameki-shogo on 2016/05/19.
  */
+@Target(TYPE)
+@Retention(RUNTIME)
 public @interface DatabaseField {
+    String tableName();
 }
--- src/main/java/info/nametake/db/DatabaseTable.java
+++ src/main/java/info/nametake/db/DatabaseTable.java
@@ -1,7 +1,17 @@
 package info.nametake.db;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.FIELD;
+import static java.lang.annotation.RetentionPolicy.RUNTIME;
+
 /**
  * Created by nameki-shogo on 2016/05/19.
  */
+@Target(FIELD)
+@Retention(RUNTIME)
 public @interface DatabaseTable {
+    String columnName();
+    boolean primaryKey() default false;
 }
