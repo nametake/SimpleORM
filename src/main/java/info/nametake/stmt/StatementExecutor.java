544ff5b9bc743949d253e8f0910a47120a418236
nametake <nametake.kyarabuki@gmail.com>
2016-05-20 12:17:32 +0900

    Create constractor to StatementExecotor

--- src/main/java/info/nametake/stmt/StatementExecutor.java
+++ src/main/java/info/nametake/stmt/StatementExecutor.java
@@ -1,16 +1,62 @@
 package info.nametake.stmt;
 
+import info.nametake.db.DatabaseTable;
+import info.nametake.exception.AnnotationException;
+
 import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
 
 /**
  * Created by nameki-shogo on 2016/05/19.
  */
 public class StatementExecutor<T> {
+
+    // 渡されたコネクション
     private Connection connection = null;
+    // 格納するモデルのクラス
     private Class<T> clazz = null;
 
-    public StatementExecutor(Connection connection, Class<T> clazz) {
+    public StatementExecutor(Connection connection, T... t) throws AnnotationException {
         this.connection = connection;
+
+        @SuppressWarnings("unchecked")
+        Class<T> clazz = (Class<T>) t.getClass().getComponentType();
         this.clazz = clazz;
+
+        if (this.clazz.getAnnotation(DatabaseTable.class) == null) {
+            throw new AnnotationException("");
+        }
     }
+
+    /**
+     * 渡されたSQLを実行する。
+     * @param sql
+     * @return モデルのリスト
+     */
+    public List<T> executeQuery(String sql) throws SQLException{
+        return null;
+    }
+
+    /**
+     * 渡されたSQLを実行する。
+     * @param sql
+     * @return (1) SQL データ操作言語(DML) 文の場合は行数、(2) 何も返さない SQL 文の場合は 0
+
+     */
+    public int executeUpdate(String sql) throws SQLException {
+        return 0;
+    }
+
+    /**
+     * 渡されたResultSetの1行をモデルにして返す。
+     * @param clazz
+     * @param resultSet
+     * @return
+     */
+    public T convertResultSetToModel(Class<T> clazz, ResultSet resultSet) {
+        return null;
+    }
+
 }
--- src/test/java/info/nametake/stmt/StatementExecutorTest.java
+++ src/test/java/info/nametake/stmt/StatementExecutorTest.java
@@ -1,11 +1,31 @@
 package info.nametake.stmt;
 
 import info.nametake.BaseDBTest;
+import info.nametake.exception.AnnotationException;
+import info.nametake.models.NotAnnotationModel;
+import info.nametake.models.User;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
 /**
  * Created by shogo on 2016/05/20.
  */
 public class StatementExecutorTest extends BaseDBTest {
+    private StatementExecutor<User> stmtExecutor;
 
+    @Before
+    public void createStmtExecutor() throws AnnotationException {
+        stmtExecutor = new StatementExecutor<User>(con);
+    }
+
+    @After
+    public void releaseStmtExecutor() {
+        stmtExecutor = null;
+    }
+
+
+    @Test(expected = AnnotationException.class)
+    public void testCheckModelDataFormat() {
+    }
 }
