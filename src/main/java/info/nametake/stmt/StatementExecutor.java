a5aa60ea651fb124e96c7128b3d77c4246304786
nametake <nametake.kyarabuki@gmail.com>
2016-05-20 10:12:50 +0900

    Change StmtExecutor interface to class

--- src/main/java/info/nametake/stmt/StatementExecutor.java
+++ src/main/java/info/nametake/stmt/StatementExecutor.java
@@ -1,7 +1,16 @@
 package info.nametake.stmt;
 
+import java.sql.Connection;
+
 /**
  * Created by nameki-shogo on 2016/05/19.
  */
-public interface StatementExecutor<T> {
+public class StatementExecutor<T> {
+    private Connection connection = null;
+    private Class<T> clazz = null;
+
+    public StatementExecutor(Connection connection, Class<T> clazz) {
+        this.connection = connection;
+        this.clazz = clazz;
+    }
 }
