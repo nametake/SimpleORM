8e3ce23ca6fba5d56a84d0391c995cd7dad9ec21
nametake <nametake.kyarabuki@gmail.com>
2016-05-22 23:48:32 +0900

    Add constructor StatementBuilder.java

--- src/main/java/info/nametake/stmt/StatementBuilder.java
+++ src/main/java/info/nametake/stmt/StatementBuilder.java
@@ -1,7 +1,23 @@
 package info.nametake.stmt;
 
+import info.nametake.dao.TableInfo;
+
+import java.sql.Connection;
+import java.sql.Statement;
+
 /**
  * Created by shogo on 2016/05/22.
  */
-public class StatementBuilder {
+public class StatementBuilder<T> {
+    private Connection connection;
+    private TableInfo<T> tableInfo;
+
+    public StatementBuilder(Connection connection, TableInfo<T> tableInfo) {
+        this.connection = connection;
+        this.tableInfo = tableInfo;
+    }
+
+    public Statement getSelectAll() {
+        return null;
+    }
 }
